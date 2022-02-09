79bd0f271711f6b937acea03a4e8ee82a47cad7a
Bastian <76102128+BastianAsmussen@users.noreply.github.com>
2022-02-09 12:52:02 +0100

    More upgrades!

--- src/tech/asmussen/Main.java
+++ src/tech/asmussen/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Modes: d, b, o, h, q");
-        System.out.println("<mode> <number>");
+        System.out.println("Modes: d (decimal), b (binary), o (octal), h (hexadecimal) or q (quit)");
+        System.out.println("Input: <mode> <number>");
 
         String rawInput = "";
 
@@ -19,48 +19,53 @@ public class Main {
             System.out.print("Enter a mode and number: ");
             rawInput = scanner.nextLine();
 
+            if (rawInput.equalsIgnoreCase("q")) {
+
+                break;
+            }
+
             String[] input = rawInput.split(" ");
 
-            System.out.println(convert(String.valueOf(input[0]).charAt(0), Integer.parseInt(input[1])));
+            System.out.println(convert(String.valueOf(input[0]).charAt(0), input[1]));
         }
 
         System.out.println("Exiting...");
     }
 
-    public static String convert(char mode, int input) {
+    public static String convert(char mode, String input) {
 
         String decimal = "";
         String binary = "";
         String octal = "";
-        String hex = "";
+        String hexadecimal = "";
 
         if (mode == 'd') { // Decimal mode.
 
             decimal = String.valueOf(input);
-            binary = Integer.toBinaryString(input);
-            octal = Integer.toOctalString(input);
-            hex = Integer.toHexString(input);
+            binary = Long.toBinaryString(Long.parseLong(input));
+            octal = Long.toOctalString(Long.parseLong(input));
+            hexadecimal = Long.toHexString(Long.parseLong(input));
 
         } else if (mode == 'b') { // Binary mode.
 
-            decimal = String.valueOf(Integer.parseInt(String.valueOf(input), 2));
+            decimal = String.valueOf(Long.parseLong(String.valueOf(input), 2));
             binary = String.valueOf(input);
-            octal = Integer.toOctalString(Integer.parseInt(decimal));
-            hex = Integer.toHexString(Integer.parseInt(decimal));
+            octal = Long.toOctalString(Long.parseLong(decimal));
+            hexadecimal = Long.toHexString(Long.parseLong(decimal));
 
         } else if (mode == 'o') { // Octal mode.
 
-            decimal = String.valueOf(Integer.parseInt(String.valueOf(input), 8));
-            binary = Integer.toBinaryString(Integer.parseInt(decimal));
+            decimal = String.valueOf(Long.parseLong(String.valueOf(input), 8));
+            binary = Long.toBinaryString(Long.parseLong(decimal));
             octal = String.valueOf(input);
-            hex = Integer.toHexString(Integer.parseInt(decimal));
+            hexadecimal = Long.toHexString(Long.parseLong(decimal));
 
         } else if (mode == 'h') { // Hex mode.
 
-            decimal = String.valueOf(Integer.parseInt(String.valueOf(input), 16));
-            binary = Integer.toBinaryString(Integer.parseInt(decimal));
-            octal = Integer.toOctalString(Integer.parseInt(decimal));
-            hex = String.valueOf(input);
+            decimal = String.valueOf(Long.parseLong(String.valueOf(input), 16));
+            binary = Long.toBinaryString(Long.parseLong(decimal));
+            octal = Long.toOctalString(Long.parseLong(decimal));
+            hexadecimal = String.valueOf(input);
 
         } else { // Unsupported mode.
 
@@ -70,6 +75,6 @@ public class Main {
 
         DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
 
-        return String.format("Decimal: %s%nBinary: %s%nOctal: %s%nHex: %s%n", decimalFormat.format(Integer.parseInt(decimal)), binary, decimalFormat.format(Integer.parseInt(octal)), hex);
+        return String.format("Decimal: %s%nBinary: %s (%d bits)%nOctal: %s%nHexadecimal: %s%n", decimalFormat.format(Long.parseLong(decimal)), binary, binary.length(), decimalFormat.format(Long.parseLong(octal)), hexadecimal);
     }
 }
