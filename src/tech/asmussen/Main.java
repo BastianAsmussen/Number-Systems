6d55928775decf437b9ba5091fff7b2fd0a49337
Bastian <76102128+BastianAsmussen@users.noreply.github.com>
2022-03-01 02:44:26 +0100

    Add files via upload

--- src/tech/asmussen/Main.java
+++ src/tech/asmussen/Main.java
@@ -1,23 +1,26 @@
 package tech.asmussen;
 
 import java.util.Scanner;
-import java.text.DecimalFormat;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        final boolean DEBUG_MODE = args.length > 0 && args[0].equalsIgnoreCase("--debug");
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Modes: d (decimal), b (binary), o (octal), h (hexadecimal) or q (quit)");
         System.out.println("Input: <mode> <number>");
 
-        String rawInput = "";
+        String rawInput;
 
-        while (!rawInput.equalsIgnoreCase("q")) {
+        while (true) {
 
-            System.out.print("Enter a mode and number: ");
+            System.out.print("\nEnter a mode and a number: ");
             rawInput = scanner.nextLine();
+
+            final long START_TIME = System.currentTimeMillis();
 
             if (rawInput.equalsIgnoreCase("q")) {
 
@@ -26,61 +29,17 @@ public class Main {
 
             String[] input = rawInput.split(" ");
 
-            System.out.println(convertValue(String.valueOf(input[0]).charAt(0), input[1]));
+            System.out.println(Systems.convertValue(String.valueOf(input[0]).charAt(0), input[1]));
+
+            if (DEBUG_MODE) {
+
+                final double END_TIME = (System.currentTimeMillis() - START_TIME) / 1_000D;
+                final String[] TIME = String.valueOf(END_TIME).split("\\.");
+
+                System.out.printf("Time: %s second(s) and %s millisecond(s).%n", TIME[0], TIME[1]);
+            }
         }
 
         System.out.println("Exiting...");
     }
-
-    public static String convertValue(char mode, String input) {
-
-        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
-
-        String decimal = "";
-        String binary = "";
-        String octal = "";
-        String hexadecimal = "";
-
-        switch (mode) {
-
-            case 'd' -> { // Decimal mode.
-
-                decimal = String.valueOf(input);
-                binary = Long.toBinaryString(Long.parseLong(input));
-                octal = Long.toOctalString(Long.parseLong(input));
-                hexadecimal = Long.toHexString(Long.parseLong(input));
-
-            } case 'b' -> { // Binary mode.
-
-                decimal = String.valueOf(Long.parseLong(String.valueOf(input), 2));
-                binary = String.valueOf(input);
-                octal = Long.toOctalString(Long.parseLong(decimal));
-                hexadecimal = Long.toHexString(Long.parseLong(decimal));
-
-            } case 'o' -> { // Octal mode.
-
-                decimal = String.valueOf(Long.parseLong(String.valueOf(input), 8));
-                binary = Long.toBinaryString(Long.parseLong(decimal));
-                octal = String.valueOf(input);
-                hexadecimal = Long.toHexString(Long.parseLong(decimal));
-
-            } case 'h' -> { // Hexadecimal mode.
-
-                decimal = String.valueOf(Long.parseLong(String.valueOf(input), 16));
-                binary = Long.toBinaryString(Long.parseLong(decimal));
-                octal = Long.toOctalString(Long.parseLong(decimal));
-                hexadecimal = String.valueOf(input);
-
-            } default -> { // Unsupported mode.
-
-                System.err.println("Unsupported conversion mode!");
-                System.exit(1);
-            }
-        }
-
-        return String.format("Decimal: %s%nBinary: %s (%d bits)%nOctal: %s%nHexadecimal: %s%n", decimalFormat.format(Long.parseLong(decimal)), binary, binary.length(), decimalFormat.format(Long.parseLong(octal)), hexadecimal.toUpperCase());
-    }
 }
-/*
-IPv6 as decimal: 65152.7338.28842.62368.4102
- */
---
+++ src/tech/asmussen/Systems.java
@@ -0,0 +1,55 @@
+package tech.asmussen;
+
+import java.text.DecimalFormat;
+
+public class Systems {
+
+    public static String convertValue(char mode, String input) {
+
+        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+
+        String decimal = "";
+        String binary = "";
+        String octal = "";
+        String hexadecimal = "";
+
+        switch (mode) {
+
+            case 'd' -> { // Decimal mode.
+
+                decimal = String.valueOf(input);
+                binary = Long.toBinaryString(Long.parseLong(input));
+                octal = Long.toOctalString(Long.parseLong(input));
+                hexadecimal = Long.toHexString(Long.parseLong(input));
+
+            } case 'b' -> { // Binary mode.
+
+                decimal = String.valueOf(Long.parseLong(String.valueOf(input), 2));
+                binary = String.valueOf(input);
+                octal = Long.toOctalString(Long.parseLong(decimal));
+                hexadecimal = Long.toHexString(Long.parseLong(decimal));
+
+            } case 'o' -> { // Octal mode.
+
+                decimal = String.valueOf(Long.parseLong(String.valueOf(input), 8));
+                binary = Long.toBinaryString(Long.parseLong(decimal));
+                octal = String.valueOf(input);
+                hexadecimal = Long.toHexString(Long.parseLong(decimal));
+
+            } case 'h' -> { // Hexadecimal mode.
+
+                decimal = String.valueOf(Long.parseLong(String.valueOf(input), 16));
+                binary = Long.toBinaryString(Long.parseLong(decimal));
+                octal = Long.toOctalString(Long.parseLong(decimal));
+                hexadecimal = String.valueOf(input);
+
+            } default -> { // Unsupported mode.
+
+                System.err.println("Unsupported conversion mode!");
+                System.exit(1);
+            }
+        }
+
+        return String.format("Decimal: %s%nBinary: %s (%d bits)%nOctal: %s%nHexadecimal: %s", decimalFormat.format(Long.parseLong(decimal)), binary, binary.length(), decimalFormat.format(Long.parseLong(octal)), hexadecimal.toUpperCase());
+    }
+}
